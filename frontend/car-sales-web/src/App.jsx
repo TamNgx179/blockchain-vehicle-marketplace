87e4de8b1bc7e122b68876ebe1e54581693dab93
ShiverKane <justasimplemutt@gmail.com>
2026-04-24 23:49:09 +0700

    add login/sign up interface

--- frontend/car-sales-web/src/App.jsx
+++ frontend/car-sales-web/src/App.jsx
@@ -1,11 +1,13 @@
+import { useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useRef } from "react";
 import Home from "./pages/Home"
 import CarDetail from "./pages/CarDetail"
 import Cars from "./pages/Cars"
 import Checkout from './pages/Checkout/Checkout';
 import { CartProvider } from "./context/CartContext"; // Import nó vào
 import Notification from './components/Notification/Notification'
-import { useRef } from "react";
+import Auth from "./pages/Auth"
 function App() {
   const notifyRef = useRef();
   return (
@@ -17,6 +19,9 @@ function App() {
           <Route path="/cars" element={<Cars />} />
           <Route path="/product/:id" element={<CarDetail />} />
           <Route path="/checkout" element={<Checkout notifyRef={notifyRef} />} />
+          <Route path="/auth" element={<Auth initialMode="login" />} />
+          <Route path="/login" element={<Auth initialMode="login" />} />
+          <Route path="/signup" element={<Auth initialMode="signup" />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
